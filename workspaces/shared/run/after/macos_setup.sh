3d9a35f21451dfd8d58ae0ffd3888eecc41baef9
Joel Friedman <joel@aclaimant.com>
2021-06-21 16:21:51 -0500

    Install rosetta

--- workspaces/shared/run/after/macos_setup.sh
+++ workspaces/shared/run/after/macos_setup.sh
@@ -2,15 +2,6 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-(
-    cd "$SCRIPT_DIR/../lib"
-
-    echo "Adding Terminal theme..."
-    if ! defaults read com.apple.Terminal "Window Settings" | grep -Fw Brian >/dev/null; then
-        open -a Terminal assets/Brian.terminal
-    fi
-)
-
 echo "Resetting QuickLook."
 set -x
 qlmanage -r
